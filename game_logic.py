68e5d9c04ad6556726be6a4708495548cc0f95db
dimandr30-prog <dimandr30@gmail.com>
2026-05-23 11:26:48 +0300

    Update game_logic.py
    added background music and other game stuff

--- game_logic.py
+++ game_logic.py
@@ -1,9 +1,14 @@
-rom pygame import *
+from pygame import *
 from random import randint, choice
 
 # ---------------- INIT ----------------
 init()
 mixer.init()
+
+# ---------------- MUSIC ----------------
+mixer.music.load("background_music.ogg")
+mixer.music.set_volume(0.6)
+mixer.music.play(-1)
 
 WIDTH, HEIGHT = 1000, 600
 FPS = 60
@@ -41,12 +46,7 @@ class Particle:
 
     def draw(self, surface):
         if self.life > 0:
-            draw.circle(
-                surface,
-                self.color,
-                (int(self.x), int(self.y)),
-                self.size
-            )
+            draw.circle(surface, self.color, (int(self.x), int(self.y)), self.size)
 
 # ---------------- STARS ----------------
 class Star:
@@ -58,15 +58,118 @@ class Star:
 
     def update(self):
         self.y += self.speed
-
         if self.y > HEIGHT:
             self.y = 0
             self.x = randint(0, WIDTH)
 
     def draw(self, surface):
-        draw.circle(
-            surface,
-            (200, 220, 255),
-            (self.x, self.y),
-            self.size
-        )
+        draw.circle(surface, (200, 220, 255), (self.x, self.y), self.size)
+
+stars = [Star() for _ in range(100)]
+particles = []
+menu_particles = []
+
+def spawn_explosion(x, y):
+    for _ in range(20):
+        particles.append(Particle(x, y))
+
+# ---------------- SPRITES ----------------
+class GameSprite(sprite.Sprite):
+    def __init__(self, color, x, y, w, h, speed):
+        super().__init__()
+        self.image = Surface((w, h))
+        self.image.fill(color)
+        self.rect = self.image.get_rect(topleft=(x, y))
+        self.speed = speed
+
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+# ---------------- PLAYER ----------------
+class Player(GameSprite):
+    def __init__(self, x, y, controls):
+        super().__init__(WHITE, x, y, 18, 120, 7)
+        self.up = controls[0]
+        self.down = controls[1]
+
+    def update(self):
+        keys = key.get_pressed()
+        if keys[self.up]:
+            self.rect.y -= self.speed
+        if keys[self.down]:
+            self.rect.y += self.speed
+        self.rect.y = max(0, min(HEIGHT - self.rect.height, self.rect.y))
+
+# ---------------- BOT ----------------
+class Bot(GameSprite):
+    def __init__(self, x, y, difficulty):
+        super().__init__(WHITE, x, y, 18, 120, 5)
+        self.difficulty = difficulty
+
+    def update(self, ball):
+        if self.difficulty == "easy":
+            speed = 4
+        elif self.difficulty == "hard":
+            speed = 9
+        elif self.difficulty == "infinite":
+            speed = 12
+        else:
+            speed = 6
+
+        if self.rect.centery < ball.rect.centery:
+            self.rect.y += speed
+        if self.rect.centery > ball.rect.centery:
+            self.rect.y -= speed
+
+        self.rect.y = max(0, min(HEIGHT - self.rect.height, self.rect.y))
+
+# ---------------- BALL ----------------
+class Ball(GameSprite):
+    def __init__(self):
+        super().__init__(CYAN, WIDTH // 2, HEIGHT // 2, 20, 20, 5)
+        self.reset()
+
+    def reset(self):
+        self.rect.center = (WIDTH // 2, HEIGHT // 2)
+        self.speed_x = choice([-5, 5])
+        self.speed_y = choice([-5, 5])
+        self.base_speed = 5
+
+    def update(self):
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+
+        if self.rect.top <= 0 or self.rect.bottom >= HEIGHT:
+            self.speed_y *= -1
+
+    def collide(self, paddle):
+        if self.rect.colliderect(paddle.rect):
+            spawn_explosion(self.rect.centerx, self.rect.centery)
+            self.speed_x *= -1
+
+            if self.speed_x > 0:
+                self.speed_x += 0.5
+            else:
+                self.speed_x -= 0.5
+
+# ---------------- MUSIC FIX ----------------
+def update_music_volume():
+    if state == "menu":
+        mixer.music.set_volume(0.6)
+    elif state == "game":
+        mixer.music.set_volume(1.0)
+    else:
+        mixer.music.set_volume(0.7)
+
+# ---------------- FONTS ----------------
+font_big = font.SysFont("Arial", 60)
+font_small = font.SysFont("Arial", 28)
+
+# ---------------- SETTINGS ----------------
+settings = {
+    "easy": {"limit": 12, "bot": True},
+    "normal": {"limit": 12, "bot": True},
+    "hard": {"limit": 12, "bot": True},
+    "infinite": {"limit": 999999, "bot": True},
+    "2 players": {"limit": 12, "bot": False}
+}
